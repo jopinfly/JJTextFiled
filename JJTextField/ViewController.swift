cc64dd3355ed81c75a916ad9e89261badf1af245
李玉冰 <yubing.li@bitmain.com>
2019-09-04 18:26:09 +0800

    create project

---
+++ JJTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JJTextField
+//
+//  Created by yubing.li on 2019/9/4.
+//  Copyright © 2019 Matrixport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JJTextFieldTests/JJTextFieldTests.swift
@@ -0,0 +1,34 @@
+//
+//  JJTextFieldTests.swift
+//  JJTextFieldTests
+//
+//  Created by yubing.li on 2019/9/4.
+//  Copyright © 2019 Matrixport. All rights reserved.
+//
+
+import XCTest
+@testable import JJTextField
+
+class JJTextFieldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
