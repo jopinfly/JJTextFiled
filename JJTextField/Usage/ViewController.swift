a33eccb308afc4438f3fc79f80cd84ff760ac6db
李玉冰 <yubing.li@bitmain.com>
2019-09-05 10:47:43 +0800

    add usage

--- JJTextField/Usage/ViewController.swift
+++ JJTextField/Usage/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let SMSCodeInputter = JJTextField(frame: CGRect(x: 50, y: 100, width: 100, height: 50))
+        SMSCodeInputter.maxLength = 6
+        SMSCodeInputter.acceptedCharacterSet = .number
+        SMSCodeInputter.onBeginEdit = { (_) in }
+        view.addSubview(SMSCodeInputter)
+        
+        let someInputter = JJTextField(frame: .zero)
+        someInputter.maxLength = 2
+        someInputter.acceptedCharacterSet = .designate(cs: "abcdef")
+        view.addSubview(someInputter)
+        
+        let amountInputter = JJTextField(frame: .zero)
+        amountInputter.acceptedCharacterSet = .decimal
+        amountInputter.enableDecimalLimitMode = true
+        amountInputter.onEditing = { (_) in }
+        view.addSubview(amountInputter)
     }
 
 
